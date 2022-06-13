34ec09a11c0804b81c9d5ec56ebbaae71c4e7db3
Beau Smith <beau1smith@gmail.com>
2022-06-13 15:13:30 -0400

    ran rubocop to clean up formatting errors

--- hang_man.rb
+++ hang_man.rb
@@ -1,18 +1,21 @@
-require 'pry'
-require "json"
+# frozen_string_literal: true
 
+require 'rubocop'
+require 'json'
+
+# creates a hangman game
 class Game
-    def initialize
-      @answer = nil
-      @board = nil
-      @remaining_guesses = nil
-      @letters_used = []
-      @game_details = nil
-    end
+  def initialize
+    @answer = nil
+    @board = nil
+    @remaining_guesses = nil
+    @letters_used = []
+    @game_details = nil
+  end
 
   def boot_game(file)
-    if File.file?("saved_hangman.json") #file exists
-      puts "would you like to load your previous game: Y/N"
+    if File.file?('saved_hangman.json') # file exists
+      puts 'would you like to load your previous game: Y/N'
       load = gets.chomp.downcase
       if load == 'y'
         load_game_data(file)
@@ -22,39 +25,36 @@ class Game
     else
       new_game
     end
-end
+  end
 
   def load_game_data(file)
     file_data = File.read(file)
     file_string = JSON.parse(file_data)
-    
-    #set object variables
+
     @answer = file_string['answer']
-    @board=file_string['board']
-    @remaining_guesses=file_string['remaining_guesses']
-    @game_details = {'answer'=> @answer, 'board'=> @board, "remaining_guesses"=> @remaining_guesses}
-    
+    @board = file_string['board']
+    @remaining_guesses = file_string['remaining_guesses']
+    @game_details = { 'answer' => @answer, 'board' => @board, 'remaining_guesses' => @remaining_guesses }
+
     play
   end
 
   def new_game
-    #set object variables
     @answer = set_answer
-    @board = Array.new(@answer.length){"_"}
+    @board = Array.new(@answer.length) { '_' }
     @remaining_guesses = 6
-    @game_details = {'answer'=> @answer, 'board'=> @board, "remaining_guesses"=> @remaining_guesses}
-    
+    @game_details = { 'answer' => @answer, 'board' => @board, 'remaining_guesses' => @remaining_guesses }
     play
   end
 
   def delete_game
-    File.delete("saved_hangman.json") if File.exist?("saved_hangman.json")
+    File.delete('saved_hangman.json') if File.exist?('saved_hangman.json')
   end
 
   def set_answer
     words = File.open('hangman_words.txt')
-    random_word = words.select{|word| word.length > 5 and word.length < 12}.sample
-    answer = random_word[0..-2]
+    random_word = words.select { |word| word.length > 5 and word.length < 12 }.sample
+    @answer = random_word[0..-2]
   end
 
   def play
@@ -63,75 +63,68 @@ end
     display_board
     loop do
       puts "guesses left: #{@remaining_guesses}"
-      puts "enter a letter"
+      puts 'enter a letter'
       guess = gets.chomp
-      
+
       check_for_letters_in_answer(guess)
-      
+
       board_length = num_of_spaces
-      
-      if @board.count("_") == 0
-        puts "You won!"
-        delete_game
-        return
-      end
-      
-      if @remaining_guesses == 0
-        puts "You lost!"
+
+      if @board.count('_').zero?
+        puts 'You won!'
         delete_game
         return
       end
 
+      next unless @remaining_guesses.zero?
+
+      puts 'You lost!'
+      delete_game
+      return
     end
   end
 
   def check_for_letters_in_answer(guess)
-    split_answer = @answer.split("")
-    if guess == "exit"
-        serialize
-        exit
+    split_answer = @answer.split('')
+    if guess == 'exit'
+      serialize
+      exit
     elsif guess.length > 1
-        puts "please only enter a single letter"
-        return
+      puts 'please only enter a single letter'
+      return
     elsif !split_answer.include?(guess)
-        @letters_used.push(guess)
-        @remaining_guesses -= 1
+      @letters_used.push(guess)
+      @remaining_guesses -= 1
     else
-        @letters_used.push(guess)
-        split_answer.each_with_index do |letter, i|
-            if guess == letter
-                @board[i] = guess 
-            end
-        end
+      @letters_used.push(guess)
+      split_answer.each_with_index do |letter, i|
+        @board[i] = guess if guess == letter
+      end
     end
     display_board
   end
 
   def display_board
-    puts "#{@board.join}"
+    puts @board.join.to_s
     puts "Letters used: #{@letters_used}"
   end
 
   def num_of_spaces
-    @board.count{|space| space == "_"}
+    @board.count { |space| space == '_' }
   end
 
   def serialize
-    puts "would you like to save your game: Y/N"
+    puts 'would you like to save your game: Y/N'
     save = gets.chomp.downcase
-    if save == 'y'
+    case save
+    when 'y'
       saved_game = @game_details.to_json
-      File.open("saved_hangman.json", "w"){|f| f.write(saved_game)}
-      puts "your game has been saved"
-    elsif save == 'n'
+      File.open('saved_hangman.json', 'w') { |f| f.write(saved_game) }
+      puts 'your game has been saved'
+    when 'n'
       delete_game
-    else
-        return
     end
   end
 end
-
 game = Game.new
-game.boot_game("saved_hangman.json")
-
-
+game.boot_game('saved_hangman.json')
